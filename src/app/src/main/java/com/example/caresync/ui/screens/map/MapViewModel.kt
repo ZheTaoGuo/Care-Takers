fefc02ed01df6075e4e72df08b631e02371ff122
Lim Wei Jie <stabbershade@hotmail.com>
2025-03-05 13:13:21 +0800

    <feat> clinic model and view

--- src/app/src/main/java/com/example/caresync/ui/screens/map/MapViewModel.kt
+++ src/app/src/main/java/com/example/caresync/ui/screens/map/MapViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class MapViewModel : ViewModel() {
